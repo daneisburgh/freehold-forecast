c36457b04df350eba9fdfcfe24ca9068b3ca72d8
Dane Isburgh <daneisburgh@gmail.com>
2023-04-13 11:01:05 -0400

    feat: update county data retrieval

--- freeholdforecast/common/county_dfs.py
+++ freeholdforecast/common/county_dfs.py
@@ -1,3 +1,4 @@
+import gc
 import numpy as np
 import os
 import pandas as pd
@@ -16,8 +17,8 @@ from freeholdforecast.common.utils import make_directory, round_base, to_numeric
 state_counties = {
     "ohio": [
         "ohio-butler",
-        # "ohio-clermont",
-        # "ohio-hamilton",
+        "ohio-clermont",
+        "ohio-hamilton",
     ],
 }
 
@@ -35,9 +36,11 @@ def get_df_county(county, landing_directory):
 
 
 def get_df_state(task, landing_directory):
+
     df_counties = []
 
     for county in state_counties[task.state]:
+        task.logger.info(f"Getting data for {county}")
         county_landing_directory = os.path.join(landing_directory, county)
         make_directory(county_landing_directory)
 
@@ -50,11 +53,23 @@ def get_df_state(task, landing_directory):
         ignore_index=True,
     ).drop_duplicates(ignore_index=True)
 
-    def get_df_attom(data_paths, date_column):
-        dfs = []
+    min_sale_price = 0
+    df["last_sale_price"] = to_numeric(df.last_sale_price)
+    df = df.loc[(df.last_sale_price.notna()) & (df.last_sale_price > min_sale_price)]
 
-        for data_path in data_paths:
-            dfs.append(pd.read_csv(os.path.join(landing_directory, "attom", data_path), sep="\t", low_memory=False))
+    task.logger.info("Adding ATTOM data")
+
+    def get_df_attom(data_path_value, date_column):
+        dfs = []
+        attom_data_path = os.path.join(landing_directory, "attom")
+
+        for r, d, f in os.walk(attom_data_path):
+            if len(f) > 0:
+                file_name = f[0].split(".")[0]
+                data_path = os.path.join(attom_data_path, file_name, file_name + ".txt")
+
+                if data_path_value in data_path:
+                    dfs.append(pd.read_csv(data_path, sep="\t", low_memory=False))
 
         return (
             pd.concat(dfs, copy=False, ignore_index=True)
@@ -65,21 +80,22 @@ def get_df_state(task, landing_directory):
     def format_parid(df, parid_column):
         return df[parid_column].replace("(-|\.)", "", regex=True)
 
-    assessor_paths = [f"HOMESHAKE_TAXASSESSOR_000{x}/HOMESHAKE_TAXASSESSOR_000{x}.txt" for x in [1, 4, 6]]
-    avm_paths = [f"HOMESHAKE_AVM_000{x}/HOMESHAKE_AVM_000{x}.txt" for x in [2, 5, 7]]
-
-    df_assessor = get_df_attom(assessor_paths, "AssrLastUpdated")
+    df_assessor = get_df_attom("ASSESSOR", "AssrLastUpdated")
     df_assessor["ParidFormatted"] = format_parid(df_assessor, "ParcelNumberRaw")
-    df_avm = get_df_attom(avm_paths, "LastUpdateDate")
+    df_avm = get_df_attom("AVM", "LastUpdateDate")
 
     assessor_columns = [
         "[ATTOM ID]",
         "ParidFormatted",
+        "AssessorLastSaleDate",
+        "AssessorLastSaleAmount",
         "YearBuilt",
         "PropertyAddressFull",
+        "PropertyAddressCity",
         "PropertyAddressZIP",
         "SitusCounty",
         "SitusStateCode",
+        "NeighborhoodCode",
         "PropertyLatitude",
         "PropertyLongitude",
         "CompanyFlag",
@@ -106,24 +122,38 @@ def get_df_state(task, landing_directory):
 
     df_assessor_avm = pd.merge(df_assessor[assessor_columns], df_avm[avm_columns], on="[ATTOM ID]", how="left")
 
+    df["ParidFormatted"] = format_parid(df, "Parid")
+    df = pd.merge(df, df_assessor_avm, on="ParidFormatted", how="inner").drop(columns=["[ATTOM ID]", "ParidFormatted"])
+
+    del df_assessor
+    del df_avm
+    del df_assessor_avm
+    gc.collect()
+
+    task.logger.info("Formatting initial raw data")
+
     for column in [
         "PropertyAddressFull",
+        "PropertyAddressCity",
         "CompanyFlag",
+        "Owner Name 1",
         "OwnerTypeDescription1",
         "PartyOwner1NameFull",
         "PropertyUseGroup",
     ]:
-        df_assessor_avm[column] = df_assessor_avm[column].str.title()
-
-    df["ParidFormatted"] = format_parid(df, "Parid")
-    df = pd.merge(df, df_assessor_avm, on="ParidFormatted", how="inner").drop(columns=["[ATTOM ID]", "ParidFormatted"])
+        df[column] = df[column].str.title()
 
     def get_value_rounded(df, base, columns):
         for column in columns:
             df[column] = to_numeric(df[column])
-            df[column + " Rounded"] = df[column].apply(lambda x: round_base(x, base))
+            df[column + "Rounded"] = df[column].apply(lambda x: round_base(x, base))
         return df
 
+    df = get_value_rounded(
+        df,
+        10,
+        ["YearBuilt"],
+    )
     df = get_value_rounded(
         df,
         500,
@@ -131,87 +161,101 @@ def get_df_state(task, landing_directory):
     )
     df = get_value_rounded(
         df,
-        1000,
+        5000,
         ["AreaLotSF"],
     )
     df = get_value_rounded(
         df,
         25000,
-        ["TaxMarketValueLand", "TaxMarketValueImprovements", "TaxMarketValueTotal"],
+        [
+            "last_sale_price",
+            "TaxMarketValueLand",
+            "TaxMarketValueImprovements",
+            "TaxMarketValueTotal",
+            "EstimatedValue",
+        ],
     )
 
     df.sort_values(by="last_sale_date", inplace=True, ignore_index=True)
 
-    # df["House #"] = to_numeric(df["House #"])
-    # df["Year Built"] = to_numeric(df["Year Built"])
-    df["last_sale_price"] = to_numeric(df["last_sale_price"])
-    # df["Stories"] = df.Stories.apply(lambda x: np.nan if pd.isna(x) else int(np.ceil(x)))
-
-    min_sale_year = datetime.now() - relativedelta(years=+100)
-    task.logger.info(f"Min sale year: {min_sale_year}")
-
-    df = df.loc[
-        (df["last_sale_price"].notna() & (df["last_sale_price"] > 1000))
-        # (df["House #"].notna() & (df["House #"] > 0) & (df["House #"] < 1000000))
-    ]
-
-    df["last_sale_date"] = pd.to_datetime(df.last_sale_date)
-    df["last_sale_year"] = df.last_sale_date.dt.year
-
     def drop_duplicate_sale_years(df_temp):
         return df_temp.drop_duplicates(subset=["Parid", "last_sale_year"], keep="last", ignore_index=True)
 
+    df["last_sale_date"] = pd.to_datetime(df.last_sale_date)
+    df["last_sale_year"] = df.last_sale_date.dt.year
     df = drop_duplicate_sale_years(df)
     df["YearBuilt"] = to_numeric(df.YearBuilt)
 
-    df_additional = pd.DataFrame(
-        Pool(task.cpu_count).map(
-            partial(
-                get_df_additional_data,
-                all_columns=df.columns,
-                ignore_columns=[
-                    "CompanyFlag",
-                    "OwnerTypeDescription1",
-                    "PartyOwner1NameFull",
-                    "ValuationDate",
-                    "EstimatedValue",
-                    "ConfidenceScore",
-                    "last_sale_price",
-                    "last_sale_date",
-                    "last_sale_year",
-                ],
-            ),
-            df.loc[df.YearBuilt.notna() & (df.YearBuilt > min_sale_year)].to_dict("records"),
-        )
-    )
+    task.logger.info("Adding additional historical sales data")
+    df_copy = df.copy()
+    df_all = df.copy()
 
-    df_all = pd.concat([df_additional, df], copy=False, ignore_index=True)
+    # for column in ["YearBuilt"]:
+    # for column in ["AssessorLastSaleDate"]:
+    for column in ["AssessorLastSaleDate", "YearBuilt"]:
+        df_additional = pd.DataFrame(
+            Pool(task.cpu_count).map(
+                partial(
+                    get_df_additional_data,
+                    all_columns=df.columns,
+                    ignore_columns=[
+                        "AssessorLastSaleDate",
+                        "AssessorLastSaleAmount",
+                        "CompanyFlag",
+                        "Owner Name 1",
+                        "OwnerTypeDescription1",
+                        "PartyOwner1NameFull",
+                        "ValuationDate",
+                        "ValidSale",
+                        "DeedType",
+                        "last_sale_price",
+                        "last_sale_priceRounded",
+                        "last_sale_date",
+                        "last_sale_year",
+                    ],
+                    previous_column=column,
+                    min_sale_price=min_sale_price,
+                ),
+                df_copy.loc[df_copy[column].notna()].to_dict("records"),
+            )
+        )
+
+        df_all = pd.concat([df_additional, df_all], ignore_index=True)
+
+    del df_copy
+    gc.collect()
+
     df_all["last_sale_date"] = pd.to_datetime(df_all.last_sale_date)
+    df_all["last_sale_price"] = to_numeric(df_all.last_sale_price)
     df_all["last_sale_year"] = df_all.last_sale_date.dt.year
     df_all = drop_duplicate_sale_years(df_all)
     df_all.sort_values(by="last_sale_date", inplace=True, ignore_index=True)
 
-    for column in [
-        "YearBuilt",
-        "PropertyAddressFull",
-        "PropertyAddressZIP",
-        "SitusCounty",
-        "SitusStateCode",
-        "PropertyLatitude",
-        "PropertyLongitude",
-        "PropertyUseGroup",
-        "BathCount",
-        "BathPartialCount",
-        "BedroomsCount",
-        "AreaBuilding",
-        "AreaLotSF",
-        "StoriesCount",
-        "TaxMarketValueLand",
-        "TaxMarketValueImprovements",
-        "TaxMarketValueTotal",
-    ]:
-        df_all[column] = df_all.groupby("Parid", as_index=False)[column].transform(lambda x: x.bfill())
-        df_all[column] = df_all.groupby("Parid", as_index=False)[column].transform(lambda x: x.ffill())
+    # task.logger.info("Imputing numeric data")
+
+    # for column in [
+    #     "YearBuilt",
+    #     "PropertyAddressFull",
+    #     "PropertyAddressZIP",
+    #     "SitusCounty",
+    #     "SitusStateCode",
+    #     "PropertyLatitude",
+    #     "PropertyLongitude",
+    #     "PropertyUseGroup",
+    #     "BathCount",
+    #     "BathPartialCount",
+    #     "BedroomsCount",
+    #     "AreaBuilding",
+    #     "AreaLotSF",
+    #     "StoriesCount",
+    #     "TaxMarketValueLand",
+    #     "TaxMarketValueImprovements",
+    #     "TaxMarketValueTotal",
+    #     "EstimatedValue",
+    #     "ConfidenceScore",
+    # ]:
+    #     df_all[column] = df_all.groupby("Parid", as_index=False)[column].transform(lambda x: x.bfill())
+    #     df_all[column] = df_all.groupby("Parid", as_index=False)[column].transform(lambda x: x.ffill())
 
     def is_business_owner(owner):
         owner_lower = "" if pd.isna(owner) else str(owner).lower()
@@ -224,17 +268,20 @@ def get_df_state(task, landing_directory):
         return 0
 
     def is_same_owner(row):
-        owner = row["Owner Name 1"]
-        last_owner = row["Last Owner Name 1"]
+        owner = row["PartyOwner1NameFull"]
+        last_owner = row["LastPartyOwner1NameFull"]
+        last_last_owner = row["LastLastPartyOwner1NameFull"]
 
         owner_string_lower = str(owner).lower()
         last_owner_string_lower = str(last_owner).lower()
+        last_last_owner_string_lower = str(last_last_owner).lower()
 
         def string_values_to_compare(string_lower):
             return [x for x in string_lower.split() if len(x) >= 4]
 
         owner_string_array = string_values_to_compare(owner_string_lower)
         last_owner_string_array = string_values_to_compare(last_owner_string_lower)
+        last_last_owner_string_array = string_values_to_compare(last_last_owner_string_lower)
 
         return (
             1
@@ -244,17 +291,42 @@ def get_df_state(task, landing_directory):
                 and (
                     owner_string_lower in last_owner_string_lower
                     or last_owner_string_lower in owner_string_lower
+                    or last_last_owner_string_lower in owner_string_lower
                     or len(list(set(owner_string_array) & set(last_owner_string_array))) >= 1
+                    or len(list(set(owner_string_array) & set(last_last_owner_string_array))) >= 1
                 )
             )
             else 0
         )
 
     def is_good_sale(row):
-        return 1 if row["business_owner"] == 0 and row["same_owner"] == 0 else 0
+        return (
+            1
+            if (
+                row["business_owner"] == 0
+                and row["last_business_owner"] == 0
+                and row["same_owner"] == 0
+                and (pd.isna(row["last_sale_price"]) or (row["last_sale_price"] < (2 * row["TaxMarketValueTotal"])))
+            )
+            else 0
+        )
 
-    df_all["Last Owner Name 1"] = df_all.groupby("Parid")["Owner Name 1"].shift()
+    task.logger.info("Filtering final raw data and adding columns")
+
+    df_all = df_all.loc[
+        ((df_all.last_sale_price.isna()) | (df_all.last_sale_price > 0))
+        & (df_all.last_sale_date.notna())
+        & (df_all.last_sale_date.dt.year >= df_all.YearBuilt)
+        & (df_all.PropertyAddressFull.notna())
+        & (df_all.TaxMarketValueTotal > 0)
+        & (df_all.TaxMarketValueTotalRounded > 0)
+        & (df_all.EstimatedValue > 0)
+    ]
+
+    df_all["LastPartyOwner1NameFull"] = df_all.groupby("Parid")["Owner Name 1"].shift()
+    df_all["LastLastPartyOwner1NameFull"] = df_all.groupby("Parid")["LastPartyOwner1NameFull"].shift()
     df_all["business_owner"] = df_all["Owner Name 1"].apply(is_business_owner)
+    df_all["last_business_owner"] = df_all["LastPartyOwner1NameFull"].apply(is_business_owner)
     df_all["same_owner"] = df_all.apply(is_same_owner, axis=1)
     df_all["good_sale"] = df_all.apply(is_good_sale, axis=1)
     df_all["next_business_owner"] = df_all.groupby("Parid").business_owner.shift(-1)
@@ -262,16 +334,24 @@ def get_df_state(task, landing_directory):
     df_all["next_good_sale"] = df_all.groupby("Parid").good_sale.shift(-1)
     df_all["next_sale_date"] = df_all.groupby("Parid").last_sale_date.shift(-1)
     df_all["next_sale_price"] = df_all.groupby("Parid").last_sale_price.shift(-1)
-    df_all = df_all.loc[df_all.last_sale_date.dt.year > min_sale_year]
+
+    for column in ["BathCount", "BedroomsCount", "AreaBuilding", "AreaLotSF", "StoriesCount"]:
+        df_all[column] = to_numeric(df_all[column])
+        df_all = df_all.loc[(df_all[column].notna()) & (df_all[column] > 0)]
 
     return df_all.astype(str)[
         [
             "Parid",
+            # "Owner Name 1",
+            # "Last Owner Name 1",
+            # "Last Last Owner Name 1",
             "YearBuilt",
             "PropertyAddressFull",
+            "PropertyAddressCity",
             "PropertyAddressZIP",
             "SitusCounty",
             "SitusStateCode",
+            "NeighborhoodCode",
             "PropertyLatitude",
             "PropertyLongitude",
             "PartyOwner1NameFull",
@@ -290,11 +370,16 @@ def get_df_state(task, landing_directory):
             "ValuationDate",
             "EstimatedValue",
             "ConfidenceScore",
-            "AreaBuilding Rounded",
-            "AreaLotSF Rounded",
-            "TaxMarketValueLand Rounded",
-            "TaxMarketValueImprovements Rounded",
-            "TaxMarketValueTotal Rounded",
+            "YearBuiltRounded",
+            "AreaBuildingRounded",
+            "AreaLotSFRounded",
+            "TaxMarketValueLandRounded",
+            "TaxMarketValueImprovementsRounded",
+            "TaxMarketValueTotalRounded",
+            "EstimatedValueRounded",
+            "last_sale_priceRounded",
+            "ValidSale",
+            "DeedType",
             "business_owner",
             "next_business_owner",
             "same_owner",
